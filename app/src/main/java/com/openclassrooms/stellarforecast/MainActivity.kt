0db7f6af90079f37fec8316ace1f8c650e73bfeb
jeremieguillot <jeremieguillot.pro@gmail.com>
2024-02-19 12:41:27 +0100

    P2C3

--- app/src/main/java/com/openclassrooms/stellarforecast/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/stellarforecast/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.openclassrooms.stellarforecast
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.openclassrooms.stellarforecast.databinding.ActivityMainBinding
+import com.openclassrooms.stellarforecast.domain.model.WeatherReportModel
+import com.openclassrooms.stellarforecast.presentation.home.HomeViewModel
+import com.openclassrooms.stellarforecast.presentation.home.WeatherAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: HomeViewModel by viewModels()
+    private val customAdapter = WeatherAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        defineRecyclerView()
+
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {
+                    updateCurrentWeather(it.forecast)
+                }
+            }
+        }
+    }
+
+    private fun updateCurrentWeather(forecast: List<WeatherReportModel>) {
+        customAdapter.submitList(forecast)
+    }
+
+    private fun defineRecyclerView() {
+        val layoutManager = LinearLayoutManager(applicationContext)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = customAdapter
     }
 }
---
+++ app/src/main/java/com/openclassrooms/stellarforecast/presentation/home/WeatherAdapter.kt
@@ -0,0 +1,59 @@
+package com.openclassrooms.stellarforecast.presentation.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.stellarforecast.databinding.ItemWeatherBinding
+import com.openclassrooms.stellarforecast.domain.model.WeatherReportModel
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class WeatherAdapter :
+    ListAdapter<WeatherReportModel, WeatherAdapter.WeatherViewHolder>(DiffCallback) {
+
+    class WeatherViewHolder(
+        private val binding: ItemWeatherBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        private val dateFormatter = SimpleDateFormat("dd/MM - HH:mm", Locale.getDefault())
+
+        fun bind(observation: WeatherReportModel) {
+            val formattedDate: String = dateFormatter.format(observation.date.time)
+            binding.textViewDateTime.text = formattedDate
+            binding.textViewStargazing.text = if (observation.isGoodForStargazing) "⭐️" else "☁️"
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
+        val itemView =
+            ItemWeatherBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return WeatherViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<WeatherReportModel>() {
+            override fun areItemsTheSame(
+                oldItem: WeatherReportModel,
+                newItem: WeatherReportModel
+            ): Boolean {
+                return oldItem.date == newItem.date
+            }
+
+
+            override fun areContentsTheSame(
+                oldItem: WeatherReportModel,
+                newItem: WeatherReportModel
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
